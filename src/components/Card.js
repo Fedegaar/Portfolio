2f7b937cda74b1fe732bd955c0e0dbed167a4bda
Fedegaar <fedegaar@gmail.com>
2022-08-09 10:03:12 -0300

    Footer

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,11 @@
 import * as React from 'react'
 import img from '../images/images.jpg'
-import { container } from './Card.module.css'
+import { container, Title } from './Card.module.css'
 
 const Card = () => {
   return (
     <div className={container}>
-      <h2>The Countries Web App</h2>
-      <img src={img} alt="better call saul" />
+      <h2 className={Title}>The Countries Web App</h2>      
     </div>
   )
 }
