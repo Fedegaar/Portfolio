12f9a30621c6bf588e44968c8bb8e2f648f2121d
Fedegaar <fedegaar@gmail.com>
2022-08-09 23:52:26 -0300

    Add card style img and description

--- src/components/CardBookstore.js
+++ src/components/CardBookstore.js
@@ -1,16 +1,16 @@
 import * as React from 'react'
-import countries from '../images/the-country.png'
+import bookstore from '../images/bookstore.png'
 import { container, Title, cardImg, cardDescription } from './Card.module.css'
 
-const Card = () => {
+const CardBookstore = () => {
   return (
     <div className={container}>
-      <img src={countries} className={cardImg}/>
-      <h2 className={Title}>The Countries Web App</h2>  
+      <img src={bookstore} className={cardImg}/>
+      <h2 className={Title}>BookStore</h2>  
       <p className={cardDescription}>Lorem ipsum dolor sit amet, vide debet partiendo duo cu. Eu suscipit urbanitas mea, agam dolore alterum ut pro, eros aperiri accusam quo ut. Ad sed malis consetetur. 
 </p>    
     </div>
   )
 }
 
-export default Card
+export default CardBookstore
