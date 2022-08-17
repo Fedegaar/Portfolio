052107e4ccf30005b6fc5ed01ba8f1225aafd063
Fedegaar <fedegaar@gmail.com>
2022-08-17 10:16:49 -0300

    About redaction

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,19 +10,21 @@ const About = () => {
         <Layout pageTitle="Federico Garcia - About me" pageContent="Soy el contenido del about">
           <div className={ContentContainer}>
             <h2 className={h2Title}>Mi mundo fue, es y será éste...</h2>
-            <p className={Description}>Si profundizo podria arrancar diciendo que desde pequeño me senti atraido por la informatica en general. Luego de eso no hubo momento en mi vida en el cual no interactue con alguna herramienta digital, ya sea por trabajo, hobbie o entretenimiento. Digamos que siempre tuve una facilidad consciente para desenvolverme en informatica<br></br>
-            <br></br>
-            Si bien tuve una iniciativa academica cursando dos años de la tecnicatura en <span>Analisis de sistemas y desarrollo de Software</span> en el instituto Sedes Sapientae en mi ciudad natal, no pude culminar mis estudios por problemas personales.
-            <br></br>
+            <div class='animate__animated animate__fadeInRight'>
+              <p className={Description}>Si profundizo podria arrancar diciendo que desde pequeño me senti atraido por la informatica en general. Luego de eso no hubo momento en mi vida en el cual no interactue con alguna herramienta digital, ya sea por trabajo, hobbie o entretenimiento. Desde siempre fui consciente de mi afinidad con la informatica.<br></br>
+              <br></br>
+              Si bien tuve una iniciativa academica cursando dos años de la tecnicatura en <span>Analisis de sistemas y desarrollo de Software</span> en el instituto Sedes Sapientae en mi ciudad natal, no pude culminar mis estudios por problemas personales.
+              <br></br>
 
-            <br></br>
-            Como <span>Full Stack Web Developer</span> busco profesionalizarme aun mas.
-            Hor por hoy con mi stack PERN, (PostgreSQL, Express, React y Node) sigo buscando que tecnologias aprender, estudiando codigo, buenas practicas, manteniendome curioso y en movimiento.<br></br>
-            <br></br>
-            Tengo inclinaciones por las mobile app's, asi como tambien por el Front-End.<br></br>
-            Soy ferviente partidario de que el desarrollo de software es un arte mas, ya que podemos dejar volar nuestra creatividad y es alli cuando logramos verdaderamente el impacto que (creo) todos buscamos..
-            De esta manera espero obtener mi primer empleo en IT, se que no va a ser facil, pero estoy convencido de lograrlo. 🚀
-            </p>
+              <br></br>
+              Como <span>Full Stack Web Developer</span> busco profesionalizarme aun mas.
+              Hor por hoy con mi stack PERN, (PostgreSQL, Express, React y Node) sigo buscando que tecnologias aprender, estudiando codigo, buenas practicas, manteniendome curioso y en movimiento.<br></br>
+              <br></br>
+              Tengo inclinaciones por las mobile app's, asi como tambien por el Front-End.<br></br>
+              Soy ferviente partidario de que el desarrollo de software es un arte mas, ya que podemos dejar volar nuestra creatividad y es alli cuando logramos verdaderamente el impacto que (creo) todos buscamos..
+              De esta manera espero obtener mi primer empleo en IT, se que no va a ser facil, pero estoy convencido de lograrlo. 🚀
+              </p>
+            </div>
             <p className={Description}>Ahora que ya sabes un poco sobre mi, te invito a ver mis proyectos...</p>
             <Link to='/projects'><a className={button} href="/projects">Go to projects</a></Link>
           </div>
