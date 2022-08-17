2e6edd31317ec9d3adf72b5ebdb13d3158734e68
Fedegaar <fedegaar@gmail.com>
2022-08-17 00:28:21 -0300

    About redaction

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,28 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import { Link } from 'gatsby'
-import { ContentContainer, Description, button } from './about.module.css'
+import SedesIMG from '../images/Sedes.png'
+import { ContentContainer, Description, button, h2Title, Sedes, SedesP } from './about.module.css'
 
 const About = () => {
   return (
     <main>        
         <Layout pageTitle="Federico Garcia - About me" pageContent="Soy el contenido del about">
           <div className={ContentContainer}>
-            <h2>Mi mundo fue, es y será éste...</h2>
-            <p>Desde muy chico tuve la suerte de sentarme en una PC, desde ahi quede encantado y ya nunca me pude alejar de este mundillo.</p>
+            <h2 className={h2Title}>Mi mundo fue, es y será éste...</h2>
+            <p className={Description}>Si profundizo podria arrancar diciendo que desde pequeño me senti atraido por la informatica en general. Luego de eso no hubo momento en mi vida en el cual no interactue con alguna herramienta digital, ya sea por trabajo, hobbie o entretenimiento. Digamos que siempre tuve una facilidad consciente para desenvolverme en informatica<br></br>
+            <br></br>
+            Si bien tuve una iniciativa academica cursando dos años de la tecnicatura en <span>Analisis de sistemas y desarrollo de Software</span> en el instituto Sedes Sapientae en mi ciudad natal, no pude culminar mis estudios por problemas personales.
+            <br></br>
+
+            <br></br>
+            Como <span>Full Stack Web Developer</span> busco profesionalizarme aun mas.
+            Hor por hoy con mi stack PERN, (PostgreSQL, Express, React y Node) sigo buscando que tecnologias aprender, estudiando codigo, buenas practicas, manteniendome curioso y en movimiento.<br></br>
+            <br></br>
+            Tengo inclinaciones por las mobile app's, asi como tambien por el Front-End.<br></br>
+            Soy ferviente partidario de que el desarrollo de software es un arte mas, ya que podemos dejar volar nuestra creatividad y es alli cuando logramos verdaderamente el impacto que (creo) todos buscamos..
+            De esta manera espero obtener mi primer empleo en IT, se que no va a ser facil, pero estoy convencido de lograrlo. 🚀
+            </p>
             <p className={Description}>Ahora que ya sabes un poco sobre mi, te invito a ver mis proyectos...</p>
             <Link to='/projects'><a className={button} href="/projects">Go to projects</a></Link>
           </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const IndexPage = () => {
       <title>Federico Garcia - Portfolio</title>
       <Layout>
         <div className={container}>          
-            <img  className={img} class="animate__animated animate__fadeInRight" alt="Profile pic" src={profile} />  
+            <img  className={img} class="animate__animated animate__fadeInLeft" alt="Profile pic" src={profile} />  
             <div   className={Info}>        
                 <h1 className={h1Name}>Hola! Soy&nbsp;<span className={name} class="animate__animated animate__fadeInRight">Federico Garcia</span></h1>
                 <p className={Description}>Un apasionado <span  className={nameF} class="animate__animated animate__fadeInUp">Full Stack Web Developer,<br/></span>  y este es mi portfolio hecho con Gatsby!</p>
@@ -20,7 +20,7 @@ const IndexPage = () => {
         </div>
         <hr/>
         <div className={ContentContainer}>
-            <p className={Description}>He incorporado el stack PERN en el bootcamp de la Academia Henry, pero siempre me mantengo aprendiendo y fortaleciendo mis skill tecnicos.</p>
+            <p className={Description}>He incorporado el stack PERN en el bootcamp de la Academia Henry, pero siempre me mantengo aprendiendo y fortaleciendo mis skills tecnicos.</p>
             <p className={Description}>Te invito a que recorras mi portfolio...</p>
             <Link to='/about'><a className={button} href="/about">¿Quien soy?</a></Link>
         </div>
