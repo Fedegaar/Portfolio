a85996af6c6f8b71708608c98aa82417fd285c0f
Fedegaar <fedegaar@gmail.com>
2022-08-21 01:23:24 -0300

    styles

--- src/components/CardBookstore.js
+++ src/components/CardBookstore.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import bookstore from '../images/bookstore.png'
 import { Link } from 'gatsby'
+import { DiSass } from 'react-icons/di'
 import 'animate.css';
 import { container, Title, cardImg, cardDescription } from './Card.module.css'
 
@@ -12,6 +13,10 @@ const CardBookstore = () => {
         <h2 className={Title}>BookStore</h2>  
         <p className={cardDescription}>BookStore es la SPA con la cual nos consagramos con mi grupo de Henry. En la misma pusimos en practica lo aprendido y aprendimos sobre la marcha varias tecnologias, frameworks y librerias de las cuales no teniamos conocimiento alguno.
         Una experiencia muy rica en la cual junto a mi equipo fortalecimos un vinculo vigente hasta el dia de hoy.. </p>    
+        <hr></hr>
+        <div>
+          <DiSass size={"30px"}/>
+        </div>
       </div>
     </Link>
   )
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,7 @@ import { Link } from 'gatsby'
 import { BsLinkedin, BsGithub } from 'react-icons/bs'
 import { HiDocumentText } from 'react-icons/hi'
 import { SiGmail } from 'react-icons/si'
-import { container, h2Title, iconsContainer, icon } from './contact.module.css'
+import { container, h2Title, iconsContainer, icon, Parrafos } from './contact.module.css'
 
 const Contact = () => {
   return (
@@ -12,12 +12,13 @@ const Contact = () => {
       <Layout pageTitle="Federico Garcia - Contacto" pageContent="Soy el contenido">
         <div className={container}>
             <h2 className={h2Title}>Pongamonos en contacto!</h2>
+            <p className={Parrafos}>Estoy en busqueda de una oportunidad en el mundo IT, de nuevos desafios que afrontar.</p>
             <div className={iconsContainer}>
               
               <Link target='none' className={icon} to='https://www.linkedin.com/in/gaarfede/'><BsLinkedin size={"30px"}/><span>LinkedIn</span></Link>
               <Link target='none' className={icon} to='https://github.com/Fedegaar'><BsGithub size={"30px"}/><span>GitHub</span></Link>
               <Link target='none' className={icon} to='mailto:fedegaar@gmail.com'><SiGmail size={"30px"}/><span>E-Mail</span></Link>
-              <Link target='none' className={icon} to='https://drive.google.com/file/d/1PLsMtNsiyeYJ3xr7g-XawFuh0tg0mkAA/view?usp=sharing'><HiDocumentText size={"34px"}/><span>Curriculum Vitae</span></Link>
+              <Link target='none' className={icon} to='https://drive.google.com/file/d/1PLsMtNsiyeYJ3xr7g-XawFuh0tg0mkAA/view?usp=sharing'><HiDocumentText size={"30px"}/><span>CV</span></Link>
             
             </div>
         </div>
