4af6a255a217647ad6b90d3c0ee916aca9ef312d
Fedegaar <fedegaar@gmail.com>
2022-08-27 16:33:34 -0300

    contact form and styles

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,8 @@ import { Link } from 'gatsby'
 import { BsLinkedin, BsGithub } from 'react-icons/bs'
 import { HiDocumentText } from 'react-icons/hi'
 import { SiGmail } from 'react-icons/si'
-import { container, h2Title, iconsContainer, icon, Parrafos } from './contact.module.css'
+import { container, h2Title, iconsContainer, icon, Parrafos, iconEmail } from './contact.module.css'
+import ContactForm from '../components/ContactForm'
 
 const Contact = () => {
   return (
@@ -12,15 +13,19 @@ const Contact = () => {
       <Layout pageTitle="Federico Garcia - Contacto" pageContent="Soy el contenido">
         <div className={container}>
             <h2 className={h2Title}>Pongamonos en contacto!</h2>
-            <p className={Parrafos}>Estoy en busqueda de una oportunidad en el mundo IT, de nuevos desafios que afrontar.</p>
-            <div className={iconsContainer}>
-              
+            <p className={Parrafos}>Estoy en busqueda de una oportunidad en el mundo IT, de nuevos desafios que afrontar.
+            Si te interesó lo que viste, tenes dudas o simplemente queres contactarte conmigo, te dejo varias opciones por las cuales contactarnos.
+            Sentite libre de usar la que te quede mas comoda...</p>
+            
+            <div className={iconsContainer}>              
               <Link target='none' className={icon} to='https://www.linkedin.com/in/gaarfede/'><BsLinkedin size={"30px"}/><span>LinkedIn</span></Link>
               <Link target='none' className={icon} to='https://github.com/Fedegaar'><BsGithub size={"30px"}/><span>GitHub</span></Link>
-              <Link target='none' className={icon} to='mailto:fedegaar@gmail.com'><SiGmail size={"30px"}/><span>E-Mail</span></Link>
+              <Link target='none' className={iconEmail} to='mailto:fedegaar@gmail.com'><SiGmail size={"30px"}/><span>E-Mail</span></Link>
               <Link target='none' className={icon} to='https://drive.google.com/file/d/1PLsMtNsiyeYJ3xr7g-XawFuh0tg0mkAA/view?usp=sharing'><HiDocumentText size={"30px"}/><span>CV</span></Link>
             
             </div>
+            <hr></hr>
+            <ContactForm/>
         </div>
       </Layout>
     </main>
